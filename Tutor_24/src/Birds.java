377fdb063910321e17ced4d3f4c8ebbff68d35d8
m.savichev <m.savichev@it24-2.tis.local>
2019-02-07 13:28:56 +0200

    Constructors tutorial.

---
+++ Tutor_24/src/Birds.java
@@ -0,0 +1,29 @@
+
+public class Birds {
+
+	private String name;
+	private boolean CanFly; 
+	
+	/**
+	 * main constructor in newbie code.
+	 * @param name
+	 * @param CanFly
+	 */
+	public Birds(String name, boolean CanFly)
+	{
+		this.name = name;
+		this.CanFly = CanFly;
+		//System.out.println("Hello.");
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public boolean isCanFly() {
+		return CanFly;
+	}
+
+	 
+	
+}
